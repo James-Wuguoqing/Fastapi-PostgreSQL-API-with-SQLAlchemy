2019584049b5329bff9647a57879279b2b1bd393
James Wuguoqing <wugq1971@gmail.com>
2023-10-04 21:24:16 +0800

    Update database.py with a new line

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEY_DATABASE_URL = "postgresql://wgq:wgq719100@hostlocal:5432/mydb"
+SQLALCHEY_DATABASE_URL = "postgresql://wgq:wgq719100@localhost:5432/mydb"
 
 Engine = create_engine(SQLALCHEY_DATABASE_URL)
 
